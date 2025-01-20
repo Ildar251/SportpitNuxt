6706fb51b4f08b2c592361cd21334f9ec12f4c87
Ildar251 <rakhmatullin-ildar@mail.ru>
2025-01-20 18:05:24 +0300

    dev

--- frontend/components/Card.vue
+++ frontend/components/Card.vue
@@ -6,3 +6,12 @@
         <slot></slot>
     </div>
 </template>
+
+
+<style lang="scss" scoped>
+.card {
+    border: 1px solid #ccc;
+    padding: 10px;
+    border-radius: 5px;
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+// Определяем интерфейс продукта
 interface Product {
     id: number
     title: string
@@ -7,20 +8,20 @@ interface Product {
 }
 
 const { find } = useStrapi()
-const { data: products } = await useAsyncData('products', async () => {
+
+const { data: products } = await useAsyncData<Product[]>('products', async () => {
     const response = await find('products', { populate: '*' })
     console.log('Ответ от Strapi:', response)
-    return response
-})
-
-
+    return response.data as Product[]
+});
 
 </script>
 
 <template>
-    <!-- <CardList v-for="product in products" :key="product.id" :product="product">
-        <Card :product="product" />
-    </CardList> -->
+    <CardList v-if="products">
+        <Card v-for="product in products" :key="product.id">
+            <h3>{{ product.title }}</h3>
+            <p>{{ product.price }}</p>
+        </Card>
+    </CardList>
 </template>
-
-<style scoped></style>
